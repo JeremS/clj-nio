675fbeb230fad203e25a94d32f5dd20f107ac012
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-07-09 16:11:49 +0200

    Added coach to run tests and a build script.

---
+++ bin/build.clj
@@ -0,0 +1,26 @@
+(ns build
+  (:require
+    [clojure.spec.test.alpha :as spec-test]
+    [com.jeremyschoffen.java.nio.alpha.file :as fs]
+    [com.jeremyschoffen.mbt.alpha.core :as mbt-core]
+    [com.jeremyschoffen.mbt.alpha.default :as mbt-defaults]
+    [com.jeremyschoffen.mbt.alpha.utils :as u]))
+
+
+(spec-test/instrument)
+
+(def specific-conf
+  (sorted-map
+    :versioning/scheme mbt-defaults/simple-scheme
+    :project/author "Jeremy Schoffen"
+    :versioning/major :alpha))
+
+(def conf (-> specific-conf
+              mbt-defaults/make-conf))
+
+
+
+(comment
+  (mbt-defaults/bump-tag!)
+  (mbt-defaults/install!)
+  (mbt-core/clean! conf))
