df967ce9b05b6271e667e9157037b53fcab53a3b
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-08-10 20:11:08 +0200

    Using new namespaces.

--- bin/build.clj
+++ bin/build.clj
@@ -1,20 +1,22 @@
 (ns build
   (:require
     [clojure.spec.test.alpha :as spec-test]
-    [com.jeremyschoffen.java.nio.alpha.file :as fs]
-    [com.jeremyschoffen.mbt.alpha.core :as mbt-core]
-    [com.jeremyschoffen.mbt.alpha.default :as mbt-defaults]
-    [com.jeremyschoffen.mbt.alpha.utils :as u]))
+    [fr.jeremyschoffen.java.nio.alpha.file :as fs]
+    [fr.jeremyschoffen.mbt.alpha.core :as mbt-core]
+    [fr.jeremyschoffen.mbt.alpha.default :as mbt-defaults]
+    [fr.jeremyschoffen.mbt.alpha.utils :as u]))
 
 
 (spec-test/instrument)
 
+
 (def specific-conf
   (sorted-map
     :project/author "Jeremy Schoffen"
-    :maven/group-id 'com.jeremyschoffen
+    :maven/group-id 'fr.jeremyschoffen
     :versioning/scheme mbt-defaults/simple-scheme
     :versioning/major :alpha))
+
 
 (def conf (-> specific-conf
               mbt-defaults/make-conf))
@@ -25,4 +27,9 @@
   (mbt-defaults/bump-tag! conf)
   (mbt-defaults/build-jar! conf)
   (mbt-defaults/install! conf)
-  (mbt-core/clean! conf))
+  (mbt-core/clean! conf)
+
+  (-> conf
+      (assoc :project/version "0")
+      (u/side-effect! mbt-defaults/build-jar!)
+      mbt-defaults/install!))
