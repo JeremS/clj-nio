8ccb8a76aafb291e396caf57cc53c1a607675f09
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-09-02 21:57:00 +0200

    Restructured build, added scm info.

--- src/build/fr/jeremyschoffen/java/nio/alpha/build.clj
+++ src/build/fr/jeremyschoffen/java/nio/alpha/build.clj
@@ -1,36 +1,36 @@
 (ns fr.jeremyschoffen.java.nio.alpha.build
   (:require
+    [clojure.spec.alpha :as s]
     [clojure.spec.test.alpha :as st]
     [fr.jeremyschoffen.mbt.alpha.core :as mbt-core]
     [fr.jeremyschoffen.mbt.alpha.default :as mbt-defaults]
     [fr.jeremyschoffen.mbt.alpha.utils :as u]
     [fr.jeremyschoffen.java.nio.alpha.docs.core :as docs]
-    [clojure.spec.alpha :as s]))
+    [build]))
+
 
 (u/pseudo-nss
   git
   maven
+  maven.scm
   project
   project.license
   versioning)
 
 
-(st/instrument `[mbt-core/deps-make-coord
-                 mbt-defaults/versioning-tag-new-version!
-                 mbt-defaults/build-jar!
-                 mbt-defaults/maven-install!
-                 mbt-defaults/maven-deploy!])
+(def conf (-> {::project/author "Jeremy Schoffen"
+               ::maven/group-id 'fr.jeremyschoffen
+               ::versioning/scheme mbt-defaults/git-distance-scheme
+               ::versioning/major :alpha
 
+               ::maven/scm {::maven.scm/url "https://github.com/JeremS/clj-nio"}
 
-(def conf {::project/author "Jeremy Schoffen"
-           ::maven/group-id 'fr.jeremyschoffen
-           ::versioning/scheme mbt-defaults/git-distance-scheme
-           ::versioning/major :alpha
-
-           ::project/licenses [{::project.license/name "Eclipse Public License - v 2.0"
-                                ::project.license/url "https://www.eclipse.org/legal/epl-v20.html"
-                                ::project.license/distribution :repo
-                                ::project.license/file (u/safer-path "LICENSE")}]})
+               ::project/licenses [{::project.license/name "Eclipse Public License - v 2.0"
+                                    ::project.license/url "https://www.eclipse.org/legal/epl-v20.html"
+                                    ::project.license/distribution :repo
+                                    ::project.license/file (u/safer-path "LICENSE")}]}
+              build/add-deploy-conf
+              mbt-defaults/config))
 
 
 (defn generate-docs! [conf]
@@ -44,31 +44,6 @@
                          ::maven/group-id
                          ::project/version]
                    :opt [::maven/classifier]})
-
-
-(def next-version+1 (mbt-defaults/versioning-make-next-version+x 1))
-
-(u/spec-op next-version+1
-           :deps [mbt-defaults/versioning-next-version]
-           :param {:req [::git/repo
-                         ::versioning/scheme]
-                   :opt [::versioning/bump-level
-                         ::versioning/tag-base-name]})
-
-
-(defn merge-version+1 [conf]
-  (-> conf
-      (u/assoc-computed ::versioning/version next-version+1
-                        ::project/version (comp str ::versioning/version))))
-
-(u/spec-op merge-version+1
-           :deps [next-version+1]
-           :param {:req [::git/repo
-                         ::versioning/scheme]
-                   :opt [::versioning/bump-level
-                         ::versioning/tag-base-name]}
-           :ret (s/keys :req [::versioning/version
-                              ::project/version]))
 
 
 (defn new-milestone! [conf]
@@ -89,5 +64,38 @@
                    :opt [::maven/classifier]})
 
 
+(st/instrument `[mbt-core/deps-make-coord
+                 mbt-defaults/versioning-tag-new-version!
+                 generate-docs!
+                 new-milestone!
+                 mbt-defaults/maven-make-github-scm
+                 mbt-defaults/build-jar!
+                 mbt-defaults/maven-deploy!])
 
 
+(defn release! []
+  (-> conf
+      (u/assoc-computed ::versioning/version mbt-defaults/versioning-next-version+1
+                        ::project/version mbt-defaults/versioning-project-version)
+      (u/do-side-effect! new-milestone!)
+      (u/assoc-computed ::maven/scm mbt-defaults/maven-make-github-scm)
+      (u/do-side-effect! mbt-defaults/build-jar!)
+      (u/do-side-effect! mbt-defaults/maven-install!)
+      u/record-build))
+
+(defn deploy! []
+  (-> conf
+      (u/assoc-computed ::versioning/version mbt-defaults/versioning-current-version
+                        ::project/version mbt-defaults/versioning-project-version)
+      (u/assoc-computed ::maven/scm mbt-defaults/maven-make-github-scm)
+      (u/do-side-effect! mbt-defaults/maven-deploy!)))
+
+
+(comment
+  (-> conf
+      (u/assoc-computed ::maven/scm mbt-defaults/maven-make-github-scm)
+      (mbt-defaults/maven-sync-pom!))
+
+  (release!)
+  (deploy!)
+  (mbt-core/clean! conf))
